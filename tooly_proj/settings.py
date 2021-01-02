388269644227de3b1bb4c3b3e77829b5e9d87763
rajabhishek9271 <rajabhishek9271@gmail.com>
2021-01-02 23:22:41 +0530

    initiated work on dashboard

--- tooly_proj/settings.py
+++ tooly_proj/settings.py
@@ -136,6 +136,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+# For allauth
+LOGIN_REDIRECT_URL = '/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_DIR = os.path.join(BASE_DIR, 'media')
