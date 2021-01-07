b4cc23c22139f2ce6c11f93f2a0e384672f59318
Naitik0909 <parmarnaitik0909@gmail.com>
2021-01-08 02:05:42 +0530

    Using sqlite3 on local host

--- tooly_proj/settings.py
+++ tooly_proj/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 env = environ.Env()
 # reading .env file
@@ -30,7 +32,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ['127.0.0.1', 'tooly-website-zlftl.ondigitalocean.app']
 
@@ -89,18 +91,28 @@ WSGI_APPLICATION = 'tooly_proj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+if os.getenv("DATABASE_URL" ,"") != "":
+    r = urlparse(os.environ.get("DATABASE_URL"))
 
-DATABASES = {
-    'default': {
-        'NAME': 'defaultdb',
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': env("DATABASE_USER"),
-        'PORT': '25060',
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
+    DATABASES = {
+        'default': {
+            'NAME': 'defaultdb',
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': r.username,
+            'PORT': r.port,
+            'PASSWORD': r.password,
+            'HOST': r.hostname,
+            'OPTIONS':{"sslmode":"require"},
 
+        }
     }
-}
+else:
+    DATABASES = {
+        'default':{
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+        }
+    }
 
 
 AUTHENTICATION_BACKENDS = [
